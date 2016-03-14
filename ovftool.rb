bc38adaad7b3563ea0260098ca2eb2a9a89f2420
Tim Lawrence <tlawrence@skyscapecloud.com>
2016-03-14 11:15:06 +0000

    added multi download of vapps

--- ovftool.rb
+++ ovftool.rb
@@ -1,30 +1,67 @@
 require 'open3'
+require 'thread'
+
 class Ovftool
   attr_accessor :source, :target
   
   
   def initialize(creds)
+    @config = creds
     self.source = creds[:source]
     file_info = File.split(self.source)
     @file_name = file_info[1]
     @file_path = file_info[0]
     @file_extension = @file_name.split(".").last
     @file_name_short = @file_name.split(".")[0]
-    @target = generate_string(creds)
+    @pass = creds[:pass]
+  end
+  
+  def upload
+    @target = generate_upload_string(@config)
     
-    perform
+    puts "Uploading: #{@file_name}"
+    puts "To: #{self.target.gsub(@pass,'REDACTED')}"
+    
+    perform(@file_name,@target)
+  end
+  
+  def download(vapp = @config[:vapp])
+    @destination_dir = @config[:directory]
+    @config[:vapp] = vapp
+    @source = generate_download_string(@config)
+    
+    puts "Downloading: #{vapp} from #{@config[:vdc]}"
+    puts "Download String: #{@source.gsub(@pass,'REDACTED')}"
+    
+    perform(@source, @destination_dir)
+  end
+  
+  def download_many(vapp_list)
+      max_threads = 5
+    
+    
+      work_q = Queue.new
+      vapp_list.map {|n| work_q.push(n)}
+      workers = (1..max_threads).map do
+        Thread.new do
+          begin
+            while vapp = work_q.pop(true)
+              download(vapp)
+            end
+          rescue ThreadError
+          end
+        end
+      end; "ok"
+      workers.map(&:join); "ok"
     
   end
   
-  def perform()
-    puts "Uploading: #{@file_name}"
-    puts "To: #{self.target.gsub(@pass,'REDACTED')}"
+  def perform(source, destination)
+
     Dir.chdir(@file_path)
-    command =  'ovftool.exe' + ' --vCloudTemplate --acceptAllEulas --allowExtraConfig --noSSLVerify '+@file_name+' '+@target
+    command =  'ovftool.exe' + ' --vCloudTemplate --acceptAllEulas --allowExtraConfig --noSSLVerify '+source+' '+destination
     
-    #out =`#{command}`
-    #puts out
-    
+
     Open3.popen3(command) do |stdin, stdout, stderr, wait_thr|
 
       until stdout.closed? do
@@ -58,8 +95,8 @@ class Ovftool
 
   end
   
-  def generate_string(creds)
-    @pass = creds[:pass]
+  def generate_upload_string(creds)
+
     dest =  '"vcloud://'+creds[:user]+':'+creds[:pass]+'@'+creds[:url]+':443?org='+creds[:org]+'&catalog='+creds[:catalogue]
     if @file_extension == 'iso'
       dest = dest + '&media='+@file_name
@@ -70,5 +107,11 @@ class Ovftool
     dest
       
   end
+  
+  def generate_download_string(creds)
+
+    source =  '"vcloud://'+creds[:user]+':'+creds[:pass]+'@'+creds[:url]+':443?org='+creds[:org]+'&vapp='+creds[:vapp]+'&vdc='+creds[:vdc]+'"'
+  end
+  
   
  end
--- upload_example.rb
+++ upload_example.rb
@@ -2,6 +2,8 @@ require '.\ovftool'
 
 vcdconfig= {
   :source => 'C:\Users\tlawrence\Downloads\coreos_production_vmware_ova.ova',
+  :directory => 'C:\Users\tlawrence\Downloads',
+  :vapp => 'name of single vapp to download',
   :user => '123.456.7890',
   :org => 'x-xxx-x-xxxx',
   :pass => 'secretpassword',
@@ -13,7 +15,17 @@ vcdconfig= {
 
 ovf = Ovftool.new(vcdconfig)
 
+#upload single ISO /OVF / OVA(as specified in :source)
 
+ovf.upload
+
+#download single vapp (as specified in :vapp)
+
+ovf.download
+
+#download multiple vApps simultaneously (from the vdc specified in :vdc)
+vapp_list = ['vapp1','vapp2','vapp3','etc']
+ovf.download_many(vapp_list)
 
 
 
